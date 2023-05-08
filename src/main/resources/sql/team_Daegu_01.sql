0ef84382f4ee8d4d11dd08838e50b33d183170ca
seungYeon <bcw5249@naver.com>
2023-05-08 13:48:31 +0900

    지역 테이블생성 승연

---
+++ src/main/resources/sql/team_Daegu_01.sql
@@ -0,0 +1,26 @@
+--여행지
+
+CREATE TABLE team_Daegu_01
+(
+  명칭 VARCHAR2(60) PRIMARY KEY,
+  우편번호 VARCHAR2(10) , 
+  주소 VARCHAR2(90) NOT NULL,
+  개요 VARCHAR2(4000) ,
+  문의및안내 VARCHAR2(80),
+  개장일 VARCHAR2(1000),
+  쉬는날 VARCHAR2(100) ,
+  체험안내 VARCHAR2(4000),
+  체험가능연령 VARCHAR2(90),
+  수용인원 VARCHAR2(100), 
+  이용시기 VARCHAR2(100) , 
+  이용시간 VARCHAR2(900),
+  주차시설 VARCHAR2(840) ,
+  유모차대여여부 VARCHAR2(6),
+  애완동물동반가능여부 VARCHAR2(6),
+  상세정보 VARCHAR2(4000)
+);
+
+
+--DROP TABLE team_Daegu_01;
+SELECT * FROM team_Daegu_01;
+
